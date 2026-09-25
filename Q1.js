27768a57d01287ae2ee20f8b0c2768382302ac3f
Ishita <ishita.iit.sharma@gmail.com>
2026-09-26 00:33:55 +0530

    Q1 done

--- Q1.js
+++ Q1.js
@@ -1,8 +1,10 @@
 const express=require("express");
 const bcrypt=require("bcryptjs")
 
+
 const app=express();
 app.use(express.json());
+
 
 const users=[
   {id:1,
@@ -17,6 +19,32 @@ const users=[
   }
 ]
 
-app.post("/login",(req,res)=>{
+
+app.post("/login",async(req,res)=>{
   const {email,password}=req.body;
+  const user=users.find((u)=>u.email===email);
+  if(!user){
+    return res.status(401).json({
+      message:"Invalid Password or email"
+    });
+  }
+    const isMatch=await bcrypt.compare(password,user.password);
+    if(!isMatch){
+      return res.status(401).json({
+        message:"Invalid Password or email"
+      })
+    }
+    return res.status(200).json({
+      message:"Login Successfull",
+      user:{
+        id:user.id,
+        email:user.email
+      }
+    })
+  
 });
+
+
+app.listen(3000,()=>{
+  console.log("Server...")
+})
