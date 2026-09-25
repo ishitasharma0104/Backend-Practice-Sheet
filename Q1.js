db4c8d7ff602054e14f9e0b19ccb8e8921494d29
Ishita <ishita.iit.sharma@gmail.com>
2026-09-26 03:35:06 +0530

    Q1 final

--- Q1.js
+++ Q1.js
@@ -1,5 +1,5 @@
-const express=require("express");
-const bcrypt=require("bcryptjs")
+let express = require("express");
+let bcrypt=require("bcryptjs");
 
 
 const app=express();
@@ -7,44 +7,44 @@ app.use(express.json());
 
 
 const users=[
-  {id:1,
-    email:"ishita@gmail.com",
-    password:(bcrypt.hashSync("12345",10))
-
+  {
+    id:1,
+    email:"ishita@gmail",
+    password: bcrypt.hashSync("12345",10)
   },
   {
     id:2,
-    email:"sneha@gmail.com",
-    password:(bcrypt.hashSync("abc123",10))
+    email:"sneha@gmail",
+    password:bcrypt.hashSync("abc123",10)
   }
 ]
 
-
-app.post("/login",async(req,res)=>{
+//login route
+app.post("/login",async (req,res)=>{
   const {email,password}=req.body;
+
+  //find user by email
   const user=users.find((u)=>u.email===email);
   if(!user){
     return res.status(401).json({
-      message:"Invalid Password or email"
-    });
-  }
-    const isMatch=await bcrypt.compare(password,user.password);
-    if(!isMatch){
-      return res.status(401).json({
-        message:"Invalid Password or email"
-      })
-    }
-    return res.status(200).json({
-      message:"Login Successfull",
-      user:{
-        id:user.id,
-        email:user.email
-      }
+      message:"Invalid..."
     })
-  
-});
-
+  }
+  const isMatch=await bcrypt.compare(password,user.password);
+  if(!isMatch){
+    return res.status(401).json({
+      message:"Invalid..."
+    })
+  }
+  return res.status(200).json({
+    message:"Success..",
+    user:{
+      id:user.id,
+      email:user.email
+    }
+  })
+})
 
 app.listen(3000,()=>{
-  console.log("Server...")
+  console.log("Server");
 })
